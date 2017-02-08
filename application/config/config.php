9088b370662f2dd28f83d73909233791d8b1efa7
Juan Rodriguez <rtjuansebastian@gmail.com>
2017-02-08 16:08:51 -0500

    Cambios para subir a producción.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/capacitaciones_movecargo';
+$config['base_url'] = 'http://172.40.18.16/capacitaciones_movecargo';
 
 /*
 |--------------------------------------------------------------------------
